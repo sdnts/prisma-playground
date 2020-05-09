23006b403d95f7071cf0647645c31726fc9c5824
Siddhant <me@madebysid.com>
2020-05-09 17:13:28 +0200

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const root = document.querySelector(".code");
+    if (root) {
+      var myCodeMirror = CodeMirror(root, {
+        value: "function myScript(){return 100;}\n",
+        lineNumbers: true,
+        matchBrackets: true,
+        mode: "text/typescript"
+      });
+    }
+  });
+</script>
+
+<section class="code" />
