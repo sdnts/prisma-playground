a448a599b26504f71000484bb7563b109fa9e215
Siddhant <me@madebysid.com>
2020-05-11 23:53:34 +0200

    Collapse all functionality into one lambda

--- packages/lambdas/execute/index.js
+++
@@ -1,6 +0,0 @@
-exports.handler = function execute(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify("/prisma"),
-  };
-};
--- packages/lambdas/migrate/index.js
+++
@@ -1,6 +0,0 @@
-exports.handler = function migrate(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify("/prisma"),
-  };
-};
