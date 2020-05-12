2a75bc0c5e8d43bb1b5aa9694187bde935b12031
Siddhant <me@madebysid.com>
2020-05-12 04:20:27 +0200

    Man I miss Typescript

--- packages/lambda/exec.js
+++ packages/lambda/exec.js
@@ -2,8 +2,8 @@ const child_process = require("child_process");
 
 module.exports = async function exec(command, options) {
   return new Promise((resolve, reject) =>
-    child_process.exec(command, options, (err, stdout, stderr) => {
-      if (err) reject({ error, stderr });
+    child_process.exec(command, options, (error, stdout, stderr) => {
+      if (error) reject({ error, stderr });
       return resolve(stdout);
     })
   );
