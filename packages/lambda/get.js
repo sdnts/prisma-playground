a339e96d95dfcb9ab951fdb557f9a1fc8da5021c
Siddhant <me@madebysid.com>
2020-05-12 03:41:09 +0200

    Improve errors from /get lambda

--- packages/lambda/get.js
+++ packages/lambda/get.js
@@ -3,15 +3,35 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = async function get(event) {
-  const { id } = event.queryStringParameters;
+  const { id } = event.queryStringParameters || {};
+
+  if (!id) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ error: "Bad Request", workspace: null }),
+    };
+  }
 
   const workspace = await prisma.workspace.findOne({ where: { id } });
+
+  if (!workspace) {
+    return {
+      statusCode: 404,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ error: "Workspace not found", workspace: null }),
+    };
+  }
 
   return {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ workspace }),
+    body: JSON.stringify({ error: null, workspace }),
   };
 };
