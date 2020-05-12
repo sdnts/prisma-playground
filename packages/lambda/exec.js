240517dc1224158c2f013fc1a13e2942809bc508
Siddhant <me@madebysid.com>
2020-05-13 01:14:46 +0200

    Log stderr and stdout from exec

--- packages/lambda/exec.js
+++ packages/lambda/exec.js
@@ -4,9 +4,13 @@ module.exports = async function exec(command, options) {
   return new Promise((resolve, reject) =>
     child_process.exec(command, options, (error, stdout, stderr) => {
       if (error) {
+        console.log('[exec] An error occurred:', error)
         console.log('[exec] stderr:', stderr)
+        console.log('[exec] stdout:', stdout)
         reject(error)
       };
+
+      console.log('[exec] stdout:', stdout)
       return resolve(stdout);
     })
   );
