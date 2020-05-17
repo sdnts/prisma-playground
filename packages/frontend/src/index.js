583338948640c8b319a2d3dd6d21c364842f2c1b
Siddhant <me@madebysid.com>
2020-05-17 22:34:09 +0200

    PLEASE

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -8,7 +8,7 @@ const app = require("./__sapper__/build/server/server");
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
 process.env.PORT = process.env.PORT || 3000;
 
-exports.handler = (event, context) => {
+exports.handler = async (event, context) => {
   console.log("Incoming Request: ", event);
   const response = await serverless(app)(event, context);
   console.log("Responding with: ", response);
