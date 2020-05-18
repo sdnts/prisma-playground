9ee2e50357ac72cbb26e9406646b08b1c17fda0a
Siddhant <me@madebysid.com>
2020-05-18 16:47:02 +0200

    Fix import in lambda handler

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -3,7 +3,7 @@
  */
 
 const serverless = require("serverless-http");
-const app = require("./__sapper__/build/server/server");
+const { default: app } = require("./__sapper__/build/server/server");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
 process.env.PORT = process.env.PORT || 3000;
