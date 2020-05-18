a01669c1feaacce04aef5f67193bf6c2c689adac
Siddhant <me@madebysid.com>
2020-05-18 17:23:40 +0200

    Well, whaddaya know, webpack works

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -8,9 +8,4 @@ const { default: app } = require("./__sapper__/build/server/server");
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
 process.env.PORT = process.env.PORT || 3000;
 
-exports.handler = async (event, context) => {
-  console.log("Incoming Request: ", event);
-  const response = await serverless(app)(event, context);
-  console.log("Responding with: ", response);
-  return response;
-};
+exports.handler = serverless(app);
